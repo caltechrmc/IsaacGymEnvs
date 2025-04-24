2c009fc0ca517f5172d26c25720bbeb685d2bc36
Ryan Rudes <ryanrudes@gmail.com>
2025-04-24 16:07:49 -0700

    Added DeXtreme on LEAP hand task

--- isaacgymenvs/tasks/__init__.py
+++ isaacgymenvs/tasks/__init__.py
@@ -45,6 +45,7 @@ from .quadcopter import Quadcopter
 from .shadow_hand import ShadowHand
 from .allegro_hand import AllegroHand
 from .dextreme.allegro_hand_dextreme import AllegroHandDextremeManualDR, AllegroHandDextremeADR
+from .dextreme.leap_hand_dextreme import LEAPHandDextremeADR
 from .trifinger import Trifinger
 
 from .allegro_kuka.allegro_kuka_reorientation import AllegroKukaReorientation
@@ -91,6 +92,7 @@ isaacgym_task_map = {
     "AllegroKukaTwoArms": resolve_allegro_kuka_two_arms,
     "AllegroHandManualDR": AllegroHandDextremeManualDR,
     "AllegroHandADR": AllegroHandDextremeADR,
+    "LEAPHandADR": LEAPHandDextremeADR,
     "Ant": Ant,
     "Anymal": Anymal,
     "AnymalTerrain": AnymalTerrain,
